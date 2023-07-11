2df52c3dd8ae907108e71afc3a7f25da2233ddfe
Andrew <50885813+SuperHeroBeard@users.noreply.github.com>
2023-07-11 15:27:50 +0100

    Update Removing Inactive Computers and Users Interactive.ps1

--- Removing Inactive Computers and Users Interactive.ps1
+++ Removing Inactive Computers and Users Interactive.ps1
@@ -1,17 +1,17 @@
 function Show-Menu {
     param (
-        [String]$Title = 'Active Directory Cleanup')
-        (
-        [String]$Info = "Highly recommend Enabling AD Recycle Bin")
+        [String]$Title = 'Active Directory Cleanup',
+        [String]$Info = "Highly recommend Enabling AD Recycle Bin"
+    )
     Clear-Host
     Write-Host "========== $Title =========="
     Write-Host "========== $Info =========="
-    Write-Host "1: Press "1" to Enable Active Directory Recycle Bin"
-    Write-Host "2: Press "2" to Display ADComputers older than 360 Days"
-    Write-Host "3: Press "3" to Delete ADComputers in a time frame you specify" 
-    Write-Host "4: Press "4" to Display ADUsers older than 360 Days" 
-    Write-Host "5: Press "5" to Delete ADUsers in a time frame you specify"
-    Write-Host "Q: Press "Q" to quit"
+    Write-Host '1: Press "1" to Enable Active Directory Recycle Bin'
+    Write-Host '2: Press "2" to Display ADComputers older than 360 Days'
+    Write-Host '3: Press "3" to Delete ADComputers in a time frame you specify'
+    Write-Host '4: Press "4" to Display ADUsers older than 360 Days'
+    Write-Host '5: Press "5" to Delete ADUsers in a time frame you specify'
+    Write-Host 'Q: Press "Q" to quit'
 }
 
 function EnableRecycleBin {
@@ -19,41 +19,30 @@ function EnableRecycleBin {
     Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target $Domain.DNSRoot -Confirm:$False -Verbose   
 }
 
-function DisplayADComputers {
-
-$DaysInactive = 360
-$InactiveDate = (Get-Date).Adddays(-($DaysInactive))
-#Get information on inactive computer accounts
-Get-ADComputer -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
+function Get-InactiveEntities {
+    param (
+        [int]$DaysInactive,
+        [String]$EntityType
+    )
+    $InactiveDate = (Get-Date).Adddays(-$DaysInactive)
+    if ($EntityType -eq 'ADComputer') {
+        Get-ADComputer -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
+    } elseif ($EntityType -eq 'ADUser') {
+        Get-ADUser -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
+    }
 }
 
-function deleteADComputers {
-$DaysInactive = Read-Host -Prompt "Enter the number of days you want to find inactive accounts"
-$InactiveDate = (Get-Date).Adddays(-($DaysInactive))
-
-$Computers = Get-ADComputer -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
-
-foreach ($item in $Computers) {
-Remove-ADObject $item.DistinguishedName
-Write-Output "$($item.DistinguishedName) - Deleted" }        
+function Delete-Entities {
+    param (
+        [int]$DaysInactive,
+        [String]$EntityType
+    )
+    $Entities = Get-InactiveEntities -DaysInactive $DaysInactive -EntityType $EntityType
+    foreach ($item in $Entities) {
+        Remove-ADObject $item.DistinguishedName
+        Write-Output "$($item.DistinguishedName) - Deleted"
+    }        
 }
-
-function DisplayAdUsers {
-$DaysInactive = 360
-$InactiveDate = (Get-Date).Adddays(-($DaysInactive))
-Get-ADUser  -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
-}
-
-function DeleteADUsers {
-    $DaysInactive = Read-Host -Prompt "Enter the number of days you want to find inactive accounts"
-    $InactiveDate = (Get-Date).Adddays(-($DaysInactive))
-    
-    $ADUsers = Get-ADUser -Filter { LastLogonDate -lt $InactiveDate -and Enabled -eq $true } -Properties LastLogonDate | Select-Object Name, LastLogonDate, DistinguishedName
-    
-    foreach ($item in $ADUsers) {
-    Remove-ADObject $item.DistinguishedName
-    Write-Output "$($item.DistinguishedName) - Deleted" }        
-    } 
 
 do 
 {
@@ -63,23 +52,28 @@ do
     {
         '1'{
             EnableRecycleBin
-            }
+        }
         '2' {  
-            DisplayADComputers
-            }
+            Get-InactiveEntities -DaysInactive 360 -EntityType 'ADComputer'
+        }
         '3' {
-            deleteADComputers
-            }
+            $DaysInactive = Read-Host -Prompt "Enter the number of days you want to find inactive accounts"
+            Delete-Entities -DaysInactive $DaysInactive -EntityType 'ADComputer'
+        }
         '4' {
-            DisplayAdUsers
-            }
+            Get-InactiveEntities -DaysInactive 360 -EntityType 'ADUser'
+        }
         '5' {
-            DeleteADUsers
-            }
+            $DaysInactive = Read-Host -Prompt "Enter the number of days you want to find inactive accounts"
+            Delete-Entities -DaysInactive $DaysInactive -EntityType 'ADUser'
+        }
         'Q' {
-                return      
-            }
+            return      
+        }
+        default {
+            Write-Host "Invalid option. Please select a valid option."
+        }
     }
     Pause
 }
-Until ($input -eq 'qQ')
+Until ($input -eq 'Q')
